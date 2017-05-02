3cd964005000830bd393fa4030eab529bf15fe41
Gavin Wahl <gwahl@fusionbox.com>
2017-05-02 17:17:55 -0600

    tag for including inline SVG

--- buggy/templatetags/buggy_tags.py
+++ buggy/templatetags/buggy_tags.py
@@ -2,7 +2,9 @@ from django import template
 from django.utils.html import format_html
 from django.utils.formats import date_format
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.conf import settings
+from django.contrib import staticfiles
 
 from babel.dates import format_timedelta
 
@@ -27,3 +29,12 @@ def absolutedate(d):
         d.isoformat(),
         date_format(timezone.localtime(d), settings.DATETIME_FORMAT),
     )
+
+
+@register.simple_tag
+def svg(svg_file):
+    filename = staticfiles.finders.find(svg_file)
+    if not filename:
+        raise IOError('Static file %r not found.' % svg_file)
+    with open(filename) as f:
+        return mark_safe(f.read())
